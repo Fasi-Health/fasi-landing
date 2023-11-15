023c0e27e2ed858a3770aaf858ee25b10416c9f3
kessio <39617555+atuta@users.noreply.github.com>
2023-11-15 14:18:44 +0300

    Add career list

--- src/routes/career.tsx
+++ src/routes/career.tsx
@@ -1,12 +1,26 @@
+import { Article, ArticleContent } from "@/components/article";
 import { Layout } from "@/components/layout";
+import { Link } from "react-router-dom";
 
 export default function CareerPage() {
     return (
         <Layout>
-            <div className="flex flex-col gap-10 pl-10">
+            <main>
+            <div className="flex flex-col gap-7 pl-10">
             <p className="text-primary text-6xl font-normal font-sans">WE'RE HIRING!</p>
-            <img src="assets/illustrators/career-page-illustrator.svg" alt="Career Page Illustrator" className=" w-96" />
+            <img src="assets/illustrators/career-page-illustrator.png" alt="Career Page Illustrator" className=" w-96" />
             </div>
+            <Article>
+                <ArticleContent title="Opportunities">
+                    <p><Link className=" no-underline" to="">Full Stack developer </Link></p>
+                    <p><Link className=" no-underline" to="">Front End developer </Link></p>
+                    <p><Link className=" no-underline" to="">Back End developer </Link></p>
+                    <p><Link className=" no-underline" to="">UX Designer </Link></p>
+                    <p><Link className=" no-underline" to="">Social media Manager </Link></p>
+                </ArticleContent>
+            </Article>
+            </main>
+         
         </Layout>
     )
 
