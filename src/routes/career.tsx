3d87b197ffe70555752a8594c5425613a29ea67a
kessio <39617555+atuta@users.noreply.github.com>
2023-11-15 10:26:45 +0300

    add career-page link to footer nav-links

--- src/routes/career.tsx
+++ src/routes/career.tsx
@@ -1,12 +1,9 @@
-import { Article } from "@/components/article";
 import { Layout } from "@/components/layout";
 
 export default function CareerPage() {
     return (
         <Layout>
-            <Article>
-               
-            </Article>
+           
         </Layout>
     )
 
