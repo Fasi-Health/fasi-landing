adf465605519b22ea75f66c906227715ce604af9
kessio <39617555+atuta@users.noreply.github.com>
2023-11-15 10:46:34 +0300

    style heading text

--- src/routes/career.tsx
+++ src/routes/career.tsx
@@ -1,8 +1,13 @@
 import { Layout } from "@/components/layout";
+import { Article } from "@/components/article";
 
 export default function CareerPage() {
     return (
         <Layout>
+            <Article>
+            <p className="text-primary text-6xl align-text-top font-normal font-sans">WE'RE HIRING!</p>
+            </Article>
+            
            
         </Layout>
     )
